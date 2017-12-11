0fd2c6792979de3e9088f31413f6dc375a6bf187
goku02 <tusharraheja231994@gmail.com>
2017-12-11 13:33:27 +0530

    Skeleton Crimemap

--- db_setup.py
+++ db_setup.py
@@ -7,7 +7,7 @@ try:
 	with connection.cursor() as cursor:
 		sql = 'CREATE DATABASE IF NOT EXISTS crimemap'
 		cursor.execute(sql)
-		sql = """CREATE TABLE IF NOT EXISTS crimemap.crime(
+		sql = """CREATE TABLE IF NOT EXISTS crimemap.crimes(
 			id int NOT NULL AUTO_INCREMENT,
 			latitude float(10, 6),
 			longitude float(10, 6),
@@ -18,6 +18,6 @@ try:
 			PRIMARY KEY(id)
 		)"""
 		cursor.execute(sql)
-	connection.commit()
+		connection.commit()
 finally:
 	connection.close()	
